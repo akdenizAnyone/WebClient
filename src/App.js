ffcb8cd4bfd1640ba1cb7c269ae5a581e973d391
Furkan <furkancantavukcu98@gmail.com>
2022-05-06 01:04:24 +0300

    configuration of theme

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='bg-blue-500'>App furkandlfdkf</h1>
+      <h1 className='text-gray-light'>Test Theme</h1>
     </div>
   )
 }
