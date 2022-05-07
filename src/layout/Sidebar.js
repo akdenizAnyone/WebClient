2e8d6ca48214010d66fc464cc574885d5738b3e4
Furkan <furkancantavukcu98@gmail.com>
2022-05-07 04:54:24 +0300

    icons and buttons in sidebar adjusted

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,18 +1,51 @@
 import React from 'react'
-import { HomeIcon,ExploreIcon,NotificationIcon,MessageIcon,BookmarksIcon,ListsIcon,ProfileIcon,MoreIcon } from '../icons/icon'
+import { HomeIcon, ExploreIcon, MessageIcon, ProfileIcon } from '../icons/icon'
+import anyoneLogo from "../images/anyone.svg"
+import SideLink from "../components/SideLink"
+
+
+
+const sideLinks = [
+  {
+    name: 'Home',
+    icon: HomeIcon
+  },
+  {
+    name: 'Anonymous',
+    icon: ExploreIcon
+  },
+  {
+    name: 'Messages',
+    icon: MessageIcon
+  },
+  {
+    name: 'Profile',
+    icon: ProfileIcon
+  },
+];
 
 const Sidebar = () => {
   return (
-    <div className='w-72 bg-blue-500'>
-        <h1>Sidebar</h1>
-        <HomeIcon className={'w-7 h-7 text-red-500'}/>
-        <ExploreIcon className={'w-7 h-7 text-red-500'}/>
-        <NotificationIcon className={'w-7 h-7 text-red-500'}/>
-        <MessageIcon className={'w-7 h-7 text-red-500'}/>
-        <BookmarksIcon className={'w-7 h-7 text-red-500'}/>
-        <ListsIcon className={'w-7 h-7 text-red-500'}/>
-        <ProfileIcon className={'w-7 h-7 text-red-500'}/>
-        <MoreIcon className={'w-7 h-7 text-red-500'}/>
+    <div className='px-2 flex flex-col justify-between w-72'>
+      <div>
+        <div className='flex'>
+          <div className='mt-1 mb-24 ml-1 hover:bg-black flex items-center justify-center w-12 h-12 rounded-full transform transition-colors duration-200'>
+            <img src={anyoneLogo} alt="Anyone Logo" className='w-14 h-20' />
+          </div>
+          <a href='http://localhost:3000' className='ml-4 pt-3 font-bold text-xl'>Anyone</a>
+        </div>
+
+        <nav className='mb-16'>
+          <ul>
+            {sideLinks.map(link => (
+              <SideLink key={link.name} name={link.name} Icon={link.icon} />
+            ))}
+          </ul>
+        </nav>
+        <button className='bg-primary-base text-white rounded-full py-3 px-8 w-11/12 shadow-lg hover:bg-primary-dark transform transition-colors duration-200'>Anyweet</button>
+      </div>
+
+      <div>Bot</div>
     </div>
   )
 }
